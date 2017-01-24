23804c8df0b6d220a96dd1b7710b5cbfff9ea317
zhangzhe <xiaoyiqingz@163.com>
2017-01-24 10:46:00 +0800

    add start shell

---
+++ utils/start.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: start.sh
+# Author: zhangzhe
+# mail: xiaoyiqingz@163.com
+# Created Time: Tue 24 Jan 2017 09:52:45 AM CST
+# Desc: start jekyll in vagrant
+#########################################################################
+#!/bin/bash
+
+jekyll s -H 0.0.0.0
